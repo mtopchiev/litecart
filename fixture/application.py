0c09815389dfa098665c34d8a8304ea4b79f918a
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-20 23:46:17 +0300

    copy first fixsture files

---
+++ fixture/application.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser =="chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.driver.implicitly_wait(1)
+        self.session =SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self): #проверка активности фикстуры методом запроса url
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        driver = self.driver
+        driver.get(self.base_url)
+
+    def destroy(self):
+        self.driver.quit()# закрытие браузера
---
+++ tests/check.py
@@ -0,0 +1,7 @@
+import time
+
+
+def test(adminka):
+    time.sleep(5)
+
+#adminka.open_home_page()
