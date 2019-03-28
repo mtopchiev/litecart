ed84defcdc210b4603c5d6b11c4f17dc4a63273e
mtopchiev <47459395+mtopchiev@users.noreply.github.com>
2019-03-28 03:46:06 +0300

    Update test_add_to_cart_and_remove.py

--- tests/test_add_to_cart_and_remove.py
+++ tests/test_add_to_cart_and_remove.py
@@ -13,28 +13,45 @@ def app(request):   # создаем фикстуру
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-WebDriverWait
+
 
 def test_add_to_cart_and_remove(app):
     app.eshop_home()
     for i in range(0, 3):
-        app.driver.find_element_by_xpath("(//li[@class='product column shadow hover-light'])[1]").click()
+        app.driver.find_element_by_xpath("(//li[@class='product column shadow hover-light'])[1]").click() # открыли первый товар в рандомном списке
 
-        #qnt = app.driver.find_element_by_xpath("//span[@class='quantity'][contains(.,'0')]")
-        #qnt = app.driver.find_element_by_css_selector("div#cart span.quantity")
-        qnt = app.driver.find_element_by_css_selector("span.quantity")
+        qnt = app.driver.find_element_by_css_selector("span.quantity") # сохранили количество товаров в корзине
         intqnt = int(qnt.text)
-        listqnt = list(qnt.text)
-        nextQntStr = str(intqnt + 1)
+        nextQntStr = str(intqnt + 1)    #требуемое количество товаров в корзине после добавления 1 товара
 
-        app.driver.find_element_by_name("add_cart_product").click()
+        app.driver.find_element_by_name("add_cart_product").click() #нажимаем кнопку добавить в корзину
+        #указание ожидания до завершения анимации добавления в корзину. ждем обновления числа в счетчике корзины
         wait = WebDriverWait(app.driver, 10)  # seconds
-        #app.driver.refresh()
-      #  wait.until(EC.text_to_be_present_in_element(listqnt,nextQntStr))
-                #Element(By.cssSelector("span.quantity")), String.valueOf(count + 1)));
-
-        qnt2 = app.driver.find_element_by_xpath("//span[@class='quantity'][contains(.,'"+str(i+1)+"')]")
-
+        qnt2=By.CSS_SELECTOR,'span.quantity'
+        #wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,'span.quantity'),nextQntStr)) #строка ниже равнозначна этой
+        wait.until(EC.text_to_be_present_in_element((qnt2), nextQntStr))
         app.eshop_home()
 
-    time.sleep(5)
+    app.driver.find_element_by_xpath("//a[contains(.,'Checkout »')]").click()  # перешли в корзину
+
+    Product_in_cart = app.driver.find_element_by_xpath("//td[@style='text-align: center;']").text
+    Productint =int(Product_in_cart)
+    j=0
+    while  Productint > 0:
+        j = j + 1
+        print(j)
+        time.sleep(2)
+        app.driver.find_element_by_xpath("(//button[contains(.,'Remove')])[1]").click() # кликаем удалить 1 товар из корзины
+        # указание ожидания до завершения анимации удаления товара из корзины. ждем обновления числа в счетчике корзины
+        wait = WebDriverWait(app.driver, 10)  # seconds
+        wait.until(EC.visibility_of_element_located((By.NAME, 'remove_cart_item')))
+        Product_in_cart = app.driver.find_element_by_xpath("//td[@style='text-align: center;']").text
+        Productint = int(Product_in_cart)
+
+
+    #wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,'em'),"There are no items in your cart."))
+
+
+
+
+
